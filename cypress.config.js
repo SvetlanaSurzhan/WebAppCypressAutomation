3ff68bc6b6823b2c9dd9c49e1dcc6ec2238649f5
Svetlana Surzhan <svetlana.d.surzhan>
2024-10-21 10:53:16 -0400

    added base url

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://github.com',
+    specPattern: 'cypress/integration/**/*.{js,jsx,ts,tsx}',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
