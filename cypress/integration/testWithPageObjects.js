8b6756044b49e475e684fce7d4194772d67bd3f8
Svetlana Surzhan <svetlana.d.surzhan@gmail.com>
2024-10-23 17:10:59 -0400

    added delete repo test

--- cypress/integration/testWithPageObjects.js
+++ cypress/integration/testWithPageObjects.js
@@ -4,7 +4,19 @@ describe('Test with Page Objects', () => {
         cy.loginToApplication()
     })
 
-    it('first test', () => {
+    it('Create new repo', () => {
+        cy.get('#dashboard').find('[aria-label="Start a new repository"]').then(createNewRepo => {
+            cy.wrap(createNewRepo).get('form').find('[placeholder="name your new repository..."]').type('My first repo')
+            cy.wrap(createNewRepo).get('form').contains('button', 'Create a new repository').click()
+
+        })
+    })
+
+    it.only('Delete existing repo', () => {
+        cy.get('aside').find('[placeholder="Find a repository…"]').type('My-first-repo').then(foundRepo => {
+            cy.wrap(foundRepo).get('ul').find('li').first().click()
+            cy.wrap(foundRepo).url().should('include', '/My-first-repo')
+        })
 
     })
 })
