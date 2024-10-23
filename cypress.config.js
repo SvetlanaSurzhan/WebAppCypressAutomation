8adabbbee1034c50bb9085a535a4bbb3b6a32d0c
Svetlana Surzhan <svetlana.d.surzhan>
2024-10-23 16:04:04 -0400

    updated cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  viewportHeight: 1080,
+  viewportWidth: 1920,
+  video: false,
   e2e: {
     baseUrl: 'https://github.com',
     specPattern: 'cypress/integration/**/*.{js,jsx,ts,tsx}',
+    supportFile: 'cypress/support/e2e.{js,jsx,ts,tsx}',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
