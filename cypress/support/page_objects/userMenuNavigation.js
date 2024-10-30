6aff4d718a624d6b59f5125110d06dddeaf7ccca
Svetlana Surzhan <svetlana.d.surzhan@gmail.com>
2024-10-30 15:09:14 -0400

    refactored navigation to yourRepos test

--- cypress/support/page_objects/userMenuNavigation.js
+++ cypress/support/page_objects/userMenuNavigation.js
@@ -1,7 +1,9 @@
 export class UserMenuNavigation {
     yourRepositories() {
-        cy.get('.AppHeader-globalBar-end').find('.AppHeader-user').click()
-        cy.get('ul, [class="List__ListBox-sc-1x7olzq-0 eoXvfR"]').contains('li', 'Your repositories').click()
+        cy.get('.AppHeader-globalBar-end').find('.AppHeader-user').click().then(goToRepoPage => {
+            cy.wrap(goToRepoPage).get('ul, [class="List__ListBox-sc-1x7olzq-0 eoXvfR"]').contains('li', 'Your repositories').click()
+        })
+
         cy.url().should('include', 'repositories')
     }
     yourProfile() {
