25ac6cc4845f0625ef3d28707fad02db1c55681b
Svetlana Surzhan <svetlana.d.surzhan@gmail.com>
2024-12-17 13:34:03 -0500

    refactored userMenuNavigation

--- cypress/support/page_objects/userMenuNavigation.js
+++ cypress/support/page_objects/userMenuNavigation.js
@@ -1,13 +1,20 @@
-export class UserMenuNavigation {
-    yourRepositories() {
-        cy.get('.AppHeader-globalBar-end').find('.AppHeader-user').click()
-        cy.get('ul, [class="List__ListBox-sc-1x7olzq-0 eoXvfR"]').contains('li', 'Your repositories').click()
+function openUserMenu(menuItem) {
+    cy.get('.AppHeader-globalBar-end').find('.AppHeader-user').click()
+    cy.wait(500)
+    cy.get('[class="Dialog__Backdrop-sc-uaxjsn-0 dvTqVy"]').find('[class="List__ListBox-sc-1x7olzq-0 eoXvfR"]')
+        .contains(`${menuItem}`).click()
+    if (menuItem === 'Your profile') {
+        cy.url().should('include', 'LanaSTest')
+    } else if (menuItem === 'Your repositories') {
         cy.url().should('include', 'repositories')
     }
-    yourProfile(userName) {
-        cy.get('.AppHeader-globalBar-end').find('.AppHeader-user').click()
-        cy.get('ul, [class="List__ListBox-sc-1x7olzq-0 eoXvfR"]').contains('li', 'Your profile').click()
-        cy.url().should('include', `/${userName}`)
+}
+export class UserMenuNavigation {
+    yourProfile() {
+        openUserMenu('Your profile')
+    }
+    yourRepositories() {
+        openUserMenu('Your repositories')
     }
 }
 export const navigateFromUserMenuTo = new UserMenuNavigation()
